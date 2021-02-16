fe507170a6e43c7765cd0380796772390d2b5007
ethantjackson <ethantjackson44@gmail.com>
2021-02-16 09:01:46 -0800

    added application loop in main

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "SingletonUser.h"
+
+SingletonUser* SingletonUser::instance = 0;
+
+int main() {
+	SingletonUser* user = user->getInstance();
+	char action = user->getCurr()->run();
+	while (action != 'q') {
+		if (action == 'n') {
+			user->navigate();
+		}
+		else if (action == 's') {
+			user->save();
+		}
+		else if (action == 'u') {
+			user->reset();
+		}
+		else if (action == 'l') {
+			user->save();
+			user->login();
+			user->reset();
+		}
+		action = user->getCurr()->run();
+	}
+	user->save();
+	return 0;
+}
