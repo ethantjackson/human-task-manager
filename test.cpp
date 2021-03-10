2747585902f6b709ddc9a32e541dd631c6ed9984
36tofu <cchen233@hammer.cs.ucr.edu>
2021-03-09 18:41:16 -0800

    first test framework

--- test.cpp
+++ test.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
 #include "header/Component.h"
-#include "header/InitFactory.h"
 #include "header/Task.h"
 #include "header/TaskList.h"
+#include "header/InitFactory.h"
 #include "header/UserControl.h"
 
 #include "gtest/gtest.h"
+
+TaskList* emptyList = new TaskList("EmptyList", nullptr);
+
+TEST(taskTest, emptyList){
+	EXPECT_EQ("<tl>`EmptyList```0`</tl>`", emptyList->save());
+}
 
 int main(int argc, char** argv){
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
+
 
