cf2864cae105e41f8826e13d234beac9b93be6fd
36tofu <cchen233@hammer.cs.ucr.edu>
2021-03-09 23:49:18 -0800

    added basic Tasklist tests

--- test.cpp
+++ test.cpp
@@ -9,12 +9,26 @@
 #include "gtest/gtest.h"
 
 TaskList* emptyList = new TaskList("EmptyList", nullptr);
+TaskList* homeList = new TaskList("Home List", nullptr);
+TaskList* bobList = new TaskList("Home List", nullptr);
 
 TEST(taskTest, emptyList){
 	EXPECT_EQ("<tl>`EmptyList```0`</tl>`", emptyList->save());
 }
 
+TEST(taskTest, bobList){
+	EXPECT_EQ("<tl>`Home List```0`<tl>`Wash the car`3/10/21`Urgent!`0`</tl>`</tl>`", bobList->save());
+}
+
+TEST(taskTest, homeList){
+	EXPECT_EQ("<tl>`Home List```0`<tl>`clean the house```0`</tl>`</tl>`", homeList->save());
+	delete emptyList; delete homeList; delete bobList;
+}
+
 int main(int argc, char** argv){
+
+	homeList->appendTaskList("<tl>`clean the house```0`</tl>`");
+	bobList->appendTaskList("<tl>`Wash the car`3/10/21`Urgent!`0`</tl>`");
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
