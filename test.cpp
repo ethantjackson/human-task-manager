13ae714a0096af0c6a48d13796e0de71567cb9ac
36tofu <cchen233@hammer.cs.ucr.edu>
2021-03-10 12:11:24 -0800

    finished append tests

--- test.cpp
+++ test.cpp
@@ -12,6 +12,7 @@ TaskList* emptyList = new TaskList("EmptyList", nullptr);
 TaskList* homeList = new TaskList("Home List", nullptr);
 TaskList* bobList = new TaskList("Home List", nullptr);
 TaskList* chrisList = new TaskList("Home List", nullptr);
+TaskList* subList = new TaskList("Home List", nullptr);
 
 
 TEST(taskTest, emptyList){
@@ -26,20 +27,29 @@ TEST(taskTest, chrisList){
 	EXPECT_EQ("<tl>`Home List```0`<t>`sweep```0`</t>`<t>`mop```0`</t>`<t>`take out trash```0`</t>`<t>`clean windows```0`</t>`</tl>`", chrisList->save());
 }
 
+TEST(taskTest, subList){
+	EXPECT_EQ("<tl>`Home List```0`<tl>`subTaskList```0`</tl>`<tl>`subSubTaskList```0`</tl>`<tl>`subSubSubTaskList```0`</tl>`</tl>`", subList->save());
+}
+
 TEST(taskTest, homeList){
 	EXPECT_EQ("<tl>`Home List```0`<tl>`clean the house```0`</tl>`</tl>`", homeList->save());
-	delete emptyList; delete homeList; delete bobList; delete chrisList;
+	delete emptyList; delete homeList; delete bobList; delete chrisList; delete subList;
 }
 
 int main(int argc, char** argv){
 
 	homeList->appendTaskList("<tl>`clean the house```0`</tl>`");
+
 	bobList->appendTaskList("<tl>`Wash the car`3/10/21`Urgent!`0`</tl>`");
+
 	chrisList->appendTask("<t>`sweep```0`</t>`");
 	chrisList->appendTask("<t>`mop```0`</t>`");
 	chrisList->appendTask("<t>`take out trash```0`</t>`");
 	chrisList->appendTask("<t>`clean windows```0`</t>`");
-
+	
+	subList->appendTaskList("<tl>`subTaskList```0`</tl>`");
+	subList->appendTaskList("<tl>`subSubTaskList```0`</tl>`");
+	subList->appendTaskList("<tl>`subSubSubTaskList```0`</tl>`");
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
